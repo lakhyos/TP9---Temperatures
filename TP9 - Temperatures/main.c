fcb2a7eebcec76edd6460024cd446d6feb32ddf3
lakhyos <lakhyos@outlook.com>
2014-01-24 22:14:37 +0000

    Initial commit

---
+++ TP9 - Temperatures/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  TP9 - Temperatures
+//
+//  Created by Med Ayoub on 23/01/2014.
+//  Copyright (c) 2014 Med Ayoub. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
